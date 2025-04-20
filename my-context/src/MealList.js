d153bf75e9fb0715f143a2fc903eed644e15c68c
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-04-20 05:28:04 -0700

    Create MealList and import MealContext  and useContext

---
+++ my-context/src/MealList.js
@@ -0,0 +1,13 @@
+import { MealContext } from './Meal'
+import { useContext } from 'react'
+
+function MealList(){
+    const meals = useContext( MealContext);
+    return(
+
+        
+
+    )
+}
+
+export default MealList;
