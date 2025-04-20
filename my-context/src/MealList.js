5b160c5f3aa3126da28d862de79e7997cd9971e3
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-04-20 05:46:31 -0700

    removed the content from the App.js,  import the Meal and Meal list component. Wraped the MealList inside the meal

--- my-context/src/MealList.js
+++ my-context/src/MealList.js
@@ -4,9 +4,14 @@ import { useContext } from 'react'
 function MealList(){
     const meals = useContext( MealContext);
     return(
-
-        
-
+        <div>
+            <h1> The Menu</h1>
+            <ul>
+                {meals.map((meal, index)=>(
+                   <li key={index}>{meal}</li> 
+                ))}
+            </ul>
+        </div>
     )
 }
 
